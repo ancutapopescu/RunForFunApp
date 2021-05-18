69fa7a1f24c0747835faf0c849e9d38519193f7c
Ancuta Popescu <s_ancuta@yahoo.com>
2021-05-19 00:23:24 +0300

    Dagger-Hilt Setup

--- app/src/main/java/com/example/runforfunapp/MainActivity.kt
+++ app/src/main/java/com/example/runforfunapp/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.runforfunapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.runforfunapp.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
