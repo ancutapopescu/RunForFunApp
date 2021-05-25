e3cd1241ee79453c040635a55673d21208402343
Ancuta Popescu <s_ancuta@yahoo.com>
2021-05-25 20:48:06 +0300

    Making Our Service a Foreground Service

--- app/src/main/java/com/example/runforfunapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/runforfunapp/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.runforfunapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.runforfunapp.R
+import com.example.runforfunapp.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
 
@@ -32,5 +36,16 @@ class MainActivity : AppCompatActivity() {
                 }
             }
     }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+    if(intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+        navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+    }
+}
 
 }
