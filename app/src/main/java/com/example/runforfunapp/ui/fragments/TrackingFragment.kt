02d5124e6c47837f33054ace99d9b1edf3a98555
Ancuta Popescu <s_ancuta@yahoo.com>
2021-05-28 13:55:19 +0300

    Drawing Polylines On Our Map

--- app/src/main/java/com/example/runforfunapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runforfunapp/ui/fragments/TrackingFragment.kt
@@ -8,11 +8,20 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.runforfunapp.R
+import com.example.runforfunapp.other.Constants.ACTION_PAUSE_SERVICE
 import com.example.runforfunapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.runforfunapp.other.Constants.MAP_ZOOM
+import com.example.runforfunapp.other.Constants.POLYLINE_COLOR
+import com.example.runforfunapp.other.Constants.POLYLINE_WIDTH
+import com.example.runforfunapp.services.Polyline
 import com.example.runforfunapp.services.TrackingService
 import com.example.runforfunapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
@@ -23,18 +32,89 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private var map: GoogleMap? = null
 
+    private var isTracking = false
+    private var pathPoints = mutableListOf<Polyline>()
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         btnToggleRun.setOnClickListener {
-            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+            toggleRun()
         }
 
         mapView.onCreate(savedInstanceState)
 
         mapView.getMapAsync {
             map = it
+            addAllPolylines()
+        }
+
+        subscribeToObservers()
+    }
+
+    private fun subscribeToObservers() {
+        TrackingService.isTracking.observe(viewLifecycleOwner, Observer {
+            updateTracking(it)
+        })
+
+        TrackingService.pathPoints.observe(viewLifecycleOwner, Observer {
+            pathPoints = it
+            addLatestPolyline()
+            moveCameraToUSer()
+        })
+    }
+
+    private fun toggleRun() {
+        if(isTracking) {
+            sendCommandToService(ACTION_PAUSE_SERVICE)
+        } else {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+    }
+
+    private fun updateTracking(isTracking: Boolean) {
+        this.isTracking = isTracking
+        if(!isTracking) {
+            btnToggleRun.text = "START"
+            btnFinishRun.visibility = View.VISIBLE
+        } else {
+            btnToggleRun.text = "STOP"
+            btnFinishRun.visibility = View.GONE
+        }
+    }
+
+    private fun moveCameraToUSer() {
+        if(pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()) {
+            map?.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    pathPoints.last().last(),
+                    MAP_ZOOM
+                )
+            )
+        }
+    }
+
+    private fun addAllPolylines() {
+        for(polyline in pathPoints) {
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .addAll(polyline)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    private fun addLatestPolyline() {
+        if(pathPoints.isNotEmpty() && pathPoints.last().size > 1) {
+            val addPreLastLatLng = pathPoints.last()[pathPoints.last().size -2]
+            val addLastLatLng = pathPoints.last().last()
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .add(addPreLastLatLng)
+                .add(addLastLatLng)
+            map?.addPolyline(polylineOptions)
         }
     }
 
